994d4123f49d550af1aa3e18f28f5fbf3d5aff42
Lucian Cati <amadeuscam@yahoo.es>
2021-06-23 07:08:38 +0200

    git action file

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+
+    def setUp(self) -> None:
+        self.blog = Post.objects.create(
+            title="como mejorar como persona",
+            author="lucian cati",
+            slug="better-person"
+        )
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), "como mejorar como persona")
